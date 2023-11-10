4a525e804c9d3f3c00c532e2871fcf294d9e8d2d
James Loyd <jamestloyd@gmail.com>
2023-11-10 12:23:21 -0500

    updates

--- Quoter/Program.cs
+++ Quoter/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Quoter;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quoter;
 
 public class Program
 {
     public static async Task Main()
     {
+        var context = new QuoterContext();
+        context.Database.Migrate();
         await new Bot().MainAsync();
     }
 }
