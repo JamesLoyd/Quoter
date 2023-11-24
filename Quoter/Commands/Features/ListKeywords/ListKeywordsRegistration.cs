106aea37b50e3df4215a9250b7afc41c831b8b42
James Loyd <jamestloyd@gmail.com>
2023-11-24 14:40:58 -0500

    add registration

---
+++ Quoter/Commands/Features/ListKeywords/ListKeywordsRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Quoter.Commands.Abstractions;
+
+namespace Quoter.Commands.Features.ListKeywords;
+
+public class ListKeywordsRegistration : ICommandRegistration
+{
+    public string Name => "lk";
+    public string Description => "List all keywords";
+    public IEnumerable<CommandOption> Options { get; } = null!;
+    public Type CommandType => typeof(ListKeywordQuery);
+}
