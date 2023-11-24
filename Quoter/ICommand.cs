074b7206e63efb1a0abf14ccb3c98a83b01118ae
James Loyd <jamestloyd@gmail.com>
2023-11-24 09:56:09 -0500

    add ICommand

---
+++ Quoter/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Quoter;
+
+public interface ICommand : ICommand<Unit>
+{
+}
+
+public interface ICommand<out T> : IRequest<T>, IHasUser
+    where T : notnull
+{
+}
---
+++ Quoter/IHasUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Quoter;
+
+public interface IHasUser
+{
+    
+}
