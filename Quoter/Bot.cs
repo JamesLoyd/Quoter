666bae2f66872229c0da6f6e61ac02b32daf86c7
James Loyd <jamestloyd@gmail.com>
2023-11-25 01:32:12 -0500

    updates

--- Quoter/Bot.cs
+++ Quoter/Bot.cs
@@ -94,13 +94,6 @@ public class Bot : IBot
         // purgeQuoteCommand.AddOption("quoted", ApplicationCommandOptionType.User, "User to re-quote", true);
         // purgeQuoteCommand.AddOption("quote-id", ApplicationCommandOptionType.Number, "User to re-quote", true);
         //
-        //
-        // var addPerm = new SlashCommandBuilder();
-        // addPerm.WithName("add-perm");
-        // addPerm.WithDescription("I help you get quotes");
-        // addPerm.AddOption("permission", ApplicationCommandOptionType.Boolean, "Can purge quotes", true);
-        // addPerm.AddOption("quoted", ApplicationCommandOptionType.Role, "User to re-quote", true);
-        //
         // var deleteKeywordQuoteCommand = new SlashCommandBuilder();
         // deleteKeywordQuoteCommand.WithName("delete-quote-keyword");
         // deleteKeywordQuoteCommand.WithDescription("I help you get quotes");
@@ -111,40 +104,6 @@ public class Bot : IBot
         // listKeywordsQuoteCommand.WithDescription("I help you get quotes");
         //
         // List<ApplicationCommandProperties> applicationCommandProperties = new();
-
-
-        try
-        {
-            // Now that we have our builder, we can call the CreateApplicationCommandAsync method to make our slash command.
-            // With global commands we don't need the guild.
-            // applicationCommandProperties.Add(quoteThatCommand.Build());
-            // applicationCommandProperties.Add(requestQuoteCommand.Build());
-            // applicationCommandProperties.Add(listQuotesCommand.Build());
-            // applicationCommandProperties.Add(purgeQuotesCommand.Build());
-            // applicationCommandProperties.Add(purgeQuoteCommand.Build());
-            // applicationCommandProperties.Add(addPerm.Build());
-            // applicationCommandProperties.Add(requestQuoteKeywordCommand.Build());
-            // applicationCommandProperties.Add(deleteKeywordQuoteCommand.Build());
-            // applicationCommandProperties.Add(listKeywordsQuoteCommand.Build());
-            //
-            //
-            // await _client.BulkOverwriteGlobalApplicationCommandsAsync(applicationCommandProperties.ToArray(),
-            //     new RequestOptions
-            //     {
-            //         AuditLogReason = "Rebuild commands"
-            //     });
-
-            // Using the ready event is a simple implementation for the sake of the example. Suitable for testing and development.
-            // For a production bot, it is recommended to only run the CreateGlobalApplicationCommandAsync() once for each command.
-        }
-        catch (Exception exception)
-        {
-            // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
-            var json = JsonConvert.SerializeObject(exception, Formatting.Indented);
-
-            // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
-            Console.WriteLine(json);
-        }
     }
     
     private ICommandRegistration? GetRegistration(SocketSlashCommand command)
--- Quoter/Commands/Features/AddPermission/AddPermissionCommandRegistration.cs
+++ Quoter/Commands/Features/AddPermission/AddPermissionCommandRegistration.cs
@@ -1,11 +1,30 @@
-﻿using Quoter.Commands.Abstractions;
+﻿using Discord;
+using Quoter.Commands.Abstractions;
 
 namespace Quoter.Commands.Features.AddPermission;
 
 public class AddPermissionCommandRegistration: ICommandRegistration
 {
-    public string CommandName { get; }
-    public string Description { get; }
-    public IEnumerable<CommandOption> Options { get; }
-    public Type CommandType { get; }
+    public string CommandName => "add-perm";
+    public string Description => "The role you want to assign";
+
+    public IEnumerable<CommandOption> Options => new List<CommandOption>
+    {
+        new CommandOption
+        {
+            Description = "Can Purge quotes",
+            Name = "Permission",
+            Type = ApplicationCommandOptionType.Boolean,
+            IsRequired = true
+        },
+        new CommandOption
+        {
+            Name = "Role",
+            Description = "The role you want to add",
+            Type = ApplicationCommandOptionType.Role,
+            IsRequired = true
+        }
+    };
+
+    public Type CommandType => typeof(AddPermissionCommand);
 }
